4e35ab6889268f4483d1f4b68390b5ed8e898b06
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-10-27 12:47:17 +0200

    component

--- card-component.ts
+++ card-component.ts
@@ -1,7 +1,9 @@
 class Card extends HTMLElement {
     
     constructor() {
-        super()
+        super();
+        this.attachShadow({mode:'open'});
+        this.appendChild(this.template().content.cloneNode(true));
     }
 
     connectedCallback() {
@@ -9,6 +11,15 @@ class Card extends HTMLElement {
         
     }
 
+    template(){
+        const templat = document.createElement('template');
+        templat.innerHTML = `
+            <h1>Bild</h1>
+            <img src=""/>
+        `;
 
-
+        return templat;
+    }
 }
+
+window.customElements.define('mem-card',Card);
